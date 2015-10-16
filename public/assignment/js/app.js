98554a8f00909d5731dc6f73f3197aca820c78ad
sujith9 <sujithmahendrakar9@gmail.com>
2015-10-15 23:39:21 -0400

    Added app.js and config.js

--- public/assignment/js/app.js
+++ public/assignment/js/app.js
@@ -1,28 +1,4 @@
 (function(){
     angular
         .module("FormBuilderApp", ["ngRoute"])
-        .config(function($routeProvider){
-            $routeProvider
-                .when("/", {
-                    templateUrl: "home.html"
-                })
-                .when("/register", {
-                    templateUrl: "register.html"
-                })
-                .when("/login", {
-                    templateUrl: "login.html"
-                })
-                .when("/profile", {
-                    templateUrl: "profile.html"
-                })
-                .when("/admin", {
-                    templateUrl: "admin.html"
-                })
-                .when("/forms", {
-                    templateUrl: "forms.html"
-                })
-                .otherwise({
-                    redirectTo: "/"
-                });
-        });
 })();
